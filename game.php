7ba433a88f68212479a6449ad703c96b061ae3ba
jhastoy <45101799+jhastoy@users.noreply.github.com>
2019-03-20 16:57:18 +0100

    uo

--- game.php
+++ game.php
@@ -43,16 +43,21 @@
             }
         }
     ?>
-
-
-    <div class = "container">
-        <div class ="row justify-content-center">
-            <div id="titre_enigme" class = "col-7">
+        <div class ="row justify-content-around">
+            <div id="titre_aide" class = "col-2">
+                <h1 id="nom_enigme">AIDE</h1>
+            </div>
+            <div id="titre_enigme" class = "col-8">
                 <h1 id="nom_enigme"> <?php print(queryBDD('title','games','ID_game',$progression)); ?> </h1>
             </div>
         </div>
-        <div class = "row justify-content-center">
-            <div id ="corps_enigme" class = "col-12">
+        
+        
+        <div class = "row justify-content-around">
+        <div id ="corps_aide" class = "col-2">
+
+        </div>
+            <div id ="corps_enigme" class = "col-8">
             <?php 
             
             if($rep=='vrai' || $rep =='faux')
@@ -140,7 +145,7 @@
             }
             ?>
         </div>
-    </div>
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
